226dbe562739efd05fb31e9743d6c044a6213514
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-07 06:20:06 -0800

    adding class file

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LinkedListUC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int Choice,data,p;
+
+            Console.WriteLine("Welcome to Linked List operation");
+            LinkedList list = new LinkedList();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+
+
+        }
+    }
+}
